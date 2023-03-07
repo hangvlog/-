72d9dc1bee20da59cff6b1318180fea3bc2ed353
hangvlog <1480209511@qq.com>
2023-03-07 09:54:13 +0800

    权限设置BUG改正

--- src/router/index.js
+++ src/router/index.js
@@ -40,14 +40,13 @@ const router = new VueRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-    if (to.name == 'login') {
-        console.log(store);
-        // alert('login')
-        if (store.state.user.token) {
+    let token = store.state.user.token
+    if (token) {
+        if (to.name == 'login') {
             alert('您已经登录了，不能再次进入登录页面，请退出登录')
             // 仓库是否有用户信息
             let name = store.state.user.userInfo ? store.state.user.userInfo.name : undefined
-            console.log(name)
+            // console.log(name)
             if (!name) {
                 // alert('Please enter')
                 try {
@@ -67,7 +66,7 @@ router.beforeEach(async (to, from, next) => {
             next()
         }
     } else {
-        // next()
+        // next('/login')
         let toPath = to.path
         if (toPath.indexOf('trade') != -1 || toPath.indexOf('pay') != -1 || toPath.indexOf('center') != -1) {
             next('/login')
@@ -75,7 +74,48 @@ router.beforeEach(async (to, from, next) => {
             next()
         }
     }
-    // console.log(store);
-    // next();
 })
+// if (to.name == 'login') {
+//     console.log(store);
+//     // alert('login')
+//     if (token) {
+//         alert('您已经登录了，不能再次进入登录页面，请退出登录')
+//         // 仓库是否有用户信息
+//         let name = store.state.user.userInfo ? store.state.user.userInfo.name : undefined
+//         // console.log(name)
+//         if (!name) {
+//             // alert('Please enter')
+//             try {
+//                 await store.dispatch('getUserInfo')
+//                 next()
+//             } catch (err) {
+//                 // token过期-》移除token 重定向登录页面
+//                 await store.dispatch('logOut')
+//                 // to.name = 'login'
+//                 next('/login')
+//             }
+//         }
+//         else {
+//             next(false)
+//         }
+//     } else {
+//         next()
+//     }
+// } else {
+//     // next()
+//     if (token) {
+//         let toPath = to.path
+//         if (toPath.indexOf('trade') != -1 || toPath.indexOf('pay') != -1 || toPath.indexOf('center') != -1) {
+//             next('/login')
+//         } else {
+//             next()
+//         }
+//     }else{
+
+//     }
+
+// }
+// console.log(store);
+// next();
+// })
 export default router
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,55 +6,63 @@ import RegisterIndexVue from '@/views/Register/index.vue';
 import DetailIndexVue from '@/views/Detail/index.vue';
 import AddCartSuccess from '@/views/AddCartSuccess/index.vue'
 import ShopCart from '@/views/ShopCart/index.vue';
+import Trade from '@/views/Trade/index.vue';
 
-export default [{
-    name: 'ShopCart',
-    path: '/shopCart',
-    component: ShopCart,
-    meta: { isShown: true },
-},
-{
-    name: 'AddCartSuccess',
-    path: '/addCartSuccess',
-    component: AddCartSuccess,
-    meta: { isShown: true },
-},
-{
-    path: '/home',
-    component: HomeIndexVue,
-    meta: {
-        isShown: true,
-        // 路由方法解决三级导航展示
-        // isShoppingShown: true
+export default [
+    {
+        name: 'trade',
+        path: '/trade',
+        component: Trade,
+        meta: { isShown: false },
     },
-},
-{
-    name: 'login',
-    path: '/login',
-    component: LoginIndexVue,
-    meta: { isShown: false },
-},
-{
-    path: '/register',
-    component: RegisterIndexVue,
-    meta: { isShown: true },
-},
-{
-    name: 'search',
-    path: '/search/:keyword?',
-    component: SearchIndexVue,
-    meta: { isShown: true },
-},
-{
-    name: 'detail',
-    // name: 'detail/:skuId?',
-    path: '/detail/:skuId?',
-    component: DetailIndexVue,
-},
-// 重定向
-{
-    path: '*',
-    // component: HomeIndexVue
-    redirect: '/home'
-}
+    {
+        name: 'ShopCart',
+        path: '/shopCart',
+        component: ShopCart,
+        meta: { isShown: true },
+    },
+    {
+        name: 'AddCartSuccess',
+        path: '/addCartSuccess',
+        component: AddCartSuccess,
+        meta: { isShown: true },
+    },
+    {
+        path: '/home',
+        component: HomeIndexVue,
+        meta: {
+            isShown: true,
+            // 路由方法解决三级导航展示
+            // isShoppingShown: true
+        },
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: LoginIndexVue,
+        meta: { isShown: false },
+    },
+    {
+        path: '/register',
+        component: RegisterIndexVue,
+        meta: { isShown: true },
+    },
+    {
+        name: 'search',
+        path: '/search/:keyword?',
+        component: SearchIndexVue,
+        meta: { isShown: true },
+    },
+    {
+        name: 'detail',
+        // name: 'detail/:skuId?',
+        path: '/detail/:skuId?',
+        component: DetailIndexVue,
+    },
+    // 重定向
+    {
+        path: '*',
+        // component: HomeIndexVue
+        redirect: '/home'
+    }
 ]
